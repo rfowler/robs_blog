4a94dd16f2cf0671319a963a435dc3da355d5495
Rob Fowler <email@rfowler.co.uk>
2012-03-26 23:47:44 -0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ RobsBlog::Application.routes.draw do
   resources :comments
 
   resources :posts
+  root :to => 'posts#index'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

49d1b3a4686a46ca5d9244b8fc386ebc147355ec
Rob Fowler <email@rfowler.co.uk>
2012-03-26 23:28:00 -0400

    robs first commit to his blog yay!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RobsBlog::Application.config.secret_token = '0b2ae7b6ccc8e92dcd86e91a050a8a5bd86a9625364deb0a9bc87d3b2d768199765a43a50a1bff54aca0e813c8aed9f255916b355b86587e92e7dd46b026e0d5'
